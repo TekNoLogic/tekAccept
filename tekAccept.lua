424224ccd0e16e8a816c2e102e529692651a6644
Tekkub <tekkub@gmail.com>
2009-04-17 02:53:19 -0600

    Fix for 3.1

--- tekAccept.lua
+++ tekAccept.lua
@@ -9,10 +9,9 @@ local f = CreateFrame("Frame")
 f:RegisterEvent("PARTY_INVITE_REQUEST")
 f:SetScript("OnEvent", function(frame, event, name)
 	if IsFriend(name) then
-		AcceptGroup()
 		for i=1,STATICPOPUP_NUMDIALOGS do
 			local frame = getglobal("StaticPopup"..i)
-			if frame:IsVisible() and frame.which == "PARTY_INVITE" then frame:Hide() end
+			if frame:IsVisible() and frame.which == "PARTY_INVITE" then StaticPopup_OnClick(frame, 1) end
 		end
 	else SendWho(string.join("", "n-\"", name, "\"")) end
 end)
